5bd8dc23553184f87303381da9305e93427498a1
Asp-Codes <punetihemanth@iitbhilai.ac.in>
2024-10-01 11:34:46 +0530

    base setup of folders

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-techque
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"golang-techque/database"
+	"golang-techque/middleware"
+)
+
+var foodCollections *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+
+	//seting up the port
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	// using router gin
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoicesRoutes(router)
+
+	router.Run(":" + port)
+
+}
